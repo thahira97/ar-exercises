b3d3e09ede57e16951b112dd9fb87ec799431195
thahira97 <thahirasheerin3@gmail.com>
2023-05-04 14:36:02 -0400

    Use create to create thhe stores in DB-Ex-1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+stores = Store.create(name: "Burnaby", annual_revenue: 30000, mens_apparel: true, womens_apparel: true )
+stores = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+stores = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "Total stores count::: #{Store.all.count}"
