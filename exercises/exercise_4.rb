a3aeea5e90a6c7454233acaff75f02a16731448d
thahira97 <thahirasheerin3@gmail.com>
2023-05-04 15:01:58 -0400

    Fetch the data from DB based on a condition-Ex-4-P.1

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,14 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+stores = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true )
+stores = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+stores = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+for stores in @mens_stores 
+  puts "#{stores.name}"
+  puts "#{stores.annual_revenue}"
+end
