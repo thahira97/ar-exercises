706edfa62866a356dad82dd8dfaf5e27d65198ae
thahira97 <thahirasheerin3@gmail.com>
2023-05-04 17:18:48 -0400

    Calculate sum,avg and count-Ex-5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum("annual_revenue")
+puts "Total revenue of all stores:: #{total_revenue}"
+
+average_total_revenue = Store.average("annual_revenue")
+puts "Total average revenue of all stores:: #{average_total_revenue}"
+
+stores_making_mil = Store.where("annual_revenue >= ?", 1000000).count
+puts "stores making millions: #{stores_making_mil}"
